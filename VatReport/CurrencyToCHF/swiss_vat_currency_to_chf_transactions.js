d96da8a2ed6d7155227d6a1da8b12e960ffe9f1c
Ivan Gropetti <ivan.gropetti@gmail.com>
2017-08-07 09:24:26 +0200

    Update app

--- VatReport/CurrencyToCHF/swiss_vat_currency_to_chf_transactions.js
+++ VatReport/CurrencyToCHF/swiss_vat_currency_to_chf_transactions.js
@@ -14,7 +14,7 @@
 //
 // @id = ch.banana.addon.swissvatreport.toCHF.transactions
 // @api = 1.0
-// @pubdate = 2017-01-24
+// @pubdate = 2017-08-04
 // @publisher = Banana.ch SA
 // @description = Swiss VAT Report Transactions currency to CHF
 // @task = app.command
@@ -45,7 +45,7 @@ function loadParam(banDoc, startDate, endDate) {
     param = {
         "reportName" : "Swiss VAT Report Transactions currency to CHF",
         "bananaVersion" : "Banana Accounting 8",
-        "scriptVersion" : "script v. 2017-01-24",
+        "scriptVersion" : "script v. 2017-08-04",
         "startDate" : startDate,
         "endDate" : endDate,
         "company" : Banana.document.info("AccountingDataBase","Company"),
@@ -114,38 +114,27 @@ function createReport(banDoc, startDate, endDate) {
     tableRow.addCell("VatPostedCHF", "headerStyle", 1);
     tableRow.addCell("Tran. Curr.", "headerStyle", 1);
 
-    var tmpRowOrigin = "";
-    var rowOrigin = "";
-
     for (var i = 0; i < transactions.length; i++) {
 
-        rowOrigin = transactions[i].roworigin;
-
-        if (rowOrigin !== tmpRowOrigin) {
-
-            tableRow = table.addRow();
-            tableRow.addCell(Banana.Converter.toLocaleDateFormat(transactions[i].date), "", 1);
-            tableRow.addCell(transactions[i].doc, "", 1);
-            tableRow.addCell(transactions[i].description, "", 1);
-            tableRow.addCell(transactions[i].vatcode, "", 1);
-            tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vattaxable), "right", 1);
-            tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vatposted), "right", 1);
-            
-            var exchangerate = Banana.SDecimal.multiply(transactions[i].exchangerate,1,{'decimals':4}); //multiply to add the {decimals} option
-            tableRow.addCell(exchangerate, "right", 1);
-            
-            tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vattaxableCHF), "right", 1);
-            tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vatpostedCHF), "right", 1);
-            tableRow.addCell(transactions[i].transactioncurrency, "", 1);
-
-            totVatTaxable = Banana.SDecimal.add(totVatTaxable,transactions[i].vattaxable);
-            totVatPosted = Banana.SDecimal.add(totVatPosted,transactions[i].vatposted);
-            totVatTaxableCHF = Banana.SDecimal.add(totVatTaxableCHF,transactions[i].vattaxableCHF);
-            totVatPostedCHF = Banana.SDecimal.add(totVatPostedCHF,transactions[i].vatpostedCHF);
-
-            tmpRowOrigin = rowOrigin;
-        }
+        tableRow = table.addRow();
+        tableRow.addCell(Banana.Converter.toLocaleDateFormat(transactions[i].date), "", 1);
+        tableRow.addCell(transactions[i].doc, "", 1);
+        tableRow.addCell(transactions[i].description, "", 1);
+        tableRow.addCell(transactions[i].vatcode, "", 1);
+        tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vattaxable), "right", 1);
+        tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vatposted), "right", 1);
         
+        var exchangerate = Banana.SDecimal.multiply(transactions[i].exchangerate,1,{'decimals':4}); //multiply to add the {decimals} option
+        tableRow.addCell(exchangerate, "right", 1);
+        
+        tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vattaxableCHF), "right", 1);
+        tableRow.addCell(Banana.Converter.toLocaleNumberFormat(transactions[i].vatpostedCHF), "right", 1);
+        tableRow.addCell(transactions[i].transactioncurrency, "", 1);
+
+        totVatTaxable = Banana.SDecimal.add(totVatTaxable,transactions[i].vattaxable);
+        totVatPosted = Banana.SDecimal.add(totVatPosted,transactions[i].vatposted);
+        totVatTaxableCHF = Banana.SDecimal.add(totVatTaxableCHF,transactions[i].vattaxableCHF);
+        totVatPostedCHF = Banana.SDecimal.add(totVatPostedCHF,transactions[i].vatpostedCHF);   
     }
 
     tableRow = table.addRow();
@@ -191,10 +180,9 @@ function getJournal() {
             line.description = tRow.value("Description");
             line.transactioncurrency = tRow.value("JTransactionCurrency");
             line.isvatoperation = tRow.value("JVatIsVatOperation");
-            line.roworigin = tRow.value("JRowOrigin");
 
-            //Converts values from base currency to CHF
-            if (line.isvatoperation) { //line.vat && line.amount
+            //We take only the rows with a VAT code and then we convert values from base currency to CHF
+            if (line.isvatoperation) {
 
                 line.vattaxableCHF = convertBaseCurrencyToCHF(line.vattaxable, line.exchangerate);
                 line.vatamountCHF = convertBaseCurrencyToCHF(line.vatamount, line.exchangerate);
