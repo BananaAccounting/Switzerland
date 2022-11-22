9e4ce69c2fd8311bcf411ba8e494c92675fb9658
PloszajAaron <aaron.ploszaj@banana.ch>
2022-11-22 15:51:32 +0100

    Aggiorna filtro raiffeisen
    -aggiorna filtro
    -aggiorna test con e cambia i vecchi file con nuovi file anonimizzati

--- ImportApps/raiffeisen_import_bank_statement_csv/ch.banana.switzerland.import.raiffeisen.js
+++ ImportApps/raiffeisen_import_bank_statement_csv/ch.banana.switzerland.import.raiffeisen.js
@@ -65,8 +65,9 @@ function exec(string,isTest) {
 		return Banana.Converter.arrayToTsv(transactions);
 	}
 
-	// Format is unknow, return an error
-	return "@Error: Unknow format";
+	importUtilities.getUnknownFormatError();
+
+	return "";
 }
 
 /**
