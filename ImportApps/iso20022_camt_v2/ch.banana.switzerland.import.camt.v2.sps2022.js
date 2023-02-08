29676d785fc98f2bf516a585ca967071cd840832
Claudia Albisetti <claudia.albisetti@banana.ch>
2023-02-08 14:04:09 +0100

    EXTENS-194: Nuova versione camt 0.5x SPS 2022

---
+++ ImportApps/iso20022_camt_v2/ch.banana.switzerland.import.camt.v2.sps2022.js
@@ -0,0 +1,79 @@
+/**
+ * Define the reader for Iso 20022 camt messages 
+ * new version SPS 2022 (valid from November 2022)
+ */
+var SPS2022CamtFile = class SPS2022CamtFile extends ISO20022CamtFile {
+   constructor() {
+      super();
+      this.schema = '';
+   }
+
+   getStatementNodes() {
+      if (!this.document)
+         return [];
+
+      var statementNode = null
+      var statementNodes = [];
+      var docNode = this.document.firstChildElement(); // 'Document'
+      this.schema = docNode.attribute('xmlns');
+      return super.getStatementNodes();
+   }
+
+   readStatementEntryDetailsAddress(detailsNode, isCredit) {
+      //old versions call parent class ISO20022CamtFile
+      if (!this.schema.endsWith(".08")) {
+         return super.readStatementEntryDetailsAddress(detailsNode, isCredit);
+      }
+
+      var rltdPtiesNode = detailsNode.firstChildElement('RltdPties');
+      if (!rltdPtiesNode)
+         return '';
+   
+      var addressNode = null;
+      if (isCredit) {
+         if (rltdPtiesNode.hasChildElements('UltmtDbtr'))
+            addressNode = rltdPtiesNode.firstChildElement('UltmtDbtr')
+         else if (rltdPtiesNode.hasChildElements('Dbtr'))
+            addressNode = rltdPtiesNode.firstChildElement('Dbtr')
+         else if (rltdPtiesNode.hasChildElements('UltmtCdtr'))
+            addressNode = rltdPtiesNode.firstChildElement('UltmtCdtr')
+         else if (rltdPtiesNode.hasChildElements('Cdtr'))
+            addressNode = rltdPtiesNode.firstChildElement('Cdtr')
+      } else {
+         if (rltdPtiesNode.hasChildElements('UltmtCdtr'))
+            addressNode = rltdPtiesNode.firstChildElement('UltmtCdtr')
+         else if (rltdPtiesNode.hasChildElements('Cdtr'))
+            addressNode = rltdPtiesNode.firstChildElement('Cdtr')
+         else if (rltdPtiesNode.hasChildElements('UltmtDbtr'))
+            addressNode = rltdPtiesNode.firstChildElement('UltmtDbtr')
+         else if (rltdPtiesNode.hasChildElements('Dbtr'))
+            addressNode = rltdPtiesNode.firstChildElement('Dbtr')
+      }
+   
+      // 'Pty' new element not present in the previous version SPS2021
+      if (addressNode.hasChildElements('Pty')) {
+         addressNode = addressNode.firstChildElement('Pty');
+      }
+   
+      var addressStrings = [];
+      if (addressNode.firstChildElement('Nm'))
+         addressStrings.push(addressNode.firstChildElement('Nm').text);
+      var pstlAdrNode = addressNode.firstChildElement('PstlAdr');
+      if (pstlAdrNode) {
+         if (pstlAdrNode.hasChildElements('AdrLine')) {
+            var adrLineNode = pstlAdrNode.firstChildElement('AdrLine');
+            while(adrLineNode) {
+               addressStrings.push(adrLineNode.text);
+               adrLineNode = adrLineNode.nextSiblingElement('AdrLine');
+            }
+         }
+         if (pstlAdrNode.hasChildElements('TwnNm'))
+            addressStrings.push(pstlAdrNode.firstChildElement('TwnNm').text);
+         if (pstlAdrNode.hasChildElements('Ctry'))
+            addressStrings.push(pstlAdrNode.firstChildElement('Ctry').text);
+      }
+   
+      return addressStrings.join(', ');
+   
+   }
+}
---
+++ ImportApps/iso20022_camt_v2/changelog.md
@@ -0,0 +1,4 @@
+# Changelog
+
+2023-02-08; 
+ * [] Released new verion for SPS 2022
