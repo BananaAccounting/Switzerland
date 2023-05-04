b26ed313608b1a7c583cca0edfdab92cf0462f98
PloszajAaron <aaron.ploszaj@banana.ch>
2023-05-04 08:28:06 +0200

    Aggiorna filtro Corner Bank
    -aggiunge formato 2
    - aggiorna i test
    - code refactoring

--- ImportApps/corner_bank_transactions_csv/ch.banana.switzerland.import.corner.bank.transactions.js
+++ ImportApps/corner_bank_transactions_csv/ch.banana.switzerland.import.corner.bank.transactions.js
@@ -51,10 +51,16 @@ function exec(inData, isTest) {
   var format1 = new ImportCornerBankFormat1();
   if (format1.match( transactions))
   {
-    let intermediaryData = format1.convertCsvToIntermediaryData(transactions,convertionParam);
-    intermediaryData = format1.sortData(intermediaryData, convertionParam);
-    format1.postProcessIntermediaryData(intermediaryData);
-    return format1.convertToBananaFormat(intermediaryData);
+    transactions = format1.convert(transactions);
+    return Banana.Converter.arrayToTsv(transactions);
+  }
+
+  // Format 2
+  var format2 = new ImportCornerBankFormat2();
+  if (format2.match(transactions))
+  {
+    transactions = format2.convert(transactions);
+    return Banana.Converter.arrayToTsv(transactions);
   }
 
   // Format is unknow, return an error
@@ -128,209 +134,177 @@ var ImportCornerBankFormat1 = class ImportCornerBankFormat1 extends ImportUtilit
 
     return false;
  }
-  //Override the utilities method by adding language control
-  convertCsvToIntermediaryData(transactions, convertionParam) {
-    var form = [];
-    var intermediaryData = [];
-    var lang = "";
 
-    /** SPECIFY AT WHICH ROW OF THE CSV FILE IS THE HEADER (COLUMN TITLES)
-    We suppose the data will always begin right away after the header line */
-    convertionParam.headerLineStart = 6;
-    convertionParam.dataLineStart = 7;
-    convertionParam.title = 4;
+ /** Convert the transaction to the format to be imported */
+ convert(transactions) {
+    var transactionsToImport = [];
 
-    //Variables used to save the columns titles and the rows values
-    var columns = this.getHeaderData(transactions, convertionParam.headerLineStart); //array
-    let title= this.getTitle(transactions, convertionParam.title);
-    var rows = this.getRowData(transactions, convertionParam.dataLineStart); //array of array
-
-    //Load the form with data taken from the array. Create objects
-    this.loadForm(form, columns, rows);
-    //get the language of the headers
-    lang = this.getLanguage(title);
-    //For each row of the form, we call the rowConverter() function and we save the converted data
-    for (var i = 0; i < form.length; i++) {
-      let convertedRow = {};
-      let transaction = form[i];
-      switch(lang){
-        case "it":
-        if(transaction["Data registrazione"].match(/[0-9\/]+/g) && transaction["Data registrazione"].length == 8){
-          convertedRow = this.translateHeaderIt(transaction, convertedRow);
-          intermediaryData.push(convertedRow);
-        }
-        break;
-        case "de":
-          if(transaction["Erfassungsdatum"].match(/[0-9\/]+/g) && transaction["Erfassungsdatum"].length == 8){
-            convertedRow = this.translateHeaderDe(transaction, convertedRow);
-            intermediaryData.push(convertedRow);
+    // Filter and map rows
+    for (var i=0;i<transactions.length;i++)
+    {
+      //We take only the complete rows.
+      var transaction = transactions[i];
+      if ( transaction.length  < (this.currentLength) )
+          continue;
+      if ((transaction[this.colDate] && transaction[this.colDate].match(/[0-9\/]+/g)) &&
+          (transaction[this.colDateValuta] && transaction[this.colDateValuta].match(/[0-9\/]+/g))){
+            transactionsToImport.push(this.mapTransaction(transaction));
           }
-          break;
-        default:
-          Banana.console.info("csv format not recognised");
-      }
-  }
-    //Return the converted CSV data into the Banana document table
-    return intermediaryData;
+    }
+
+    // Sort rows by date (just invert)
+    transactionsToImport = transactionsToImport.reverse();
+
+    // Add header and return
+    var header = [["Date","DateValue","Doc","Description","Income","Expenses"]];
+    return header.concat(transactionsToImport);
   }
 
-  /**
-   * Returns the header of the file
-   * @param {*} transactions 
-   * @param {*} titleLine line with the header
-   * @returns 
-   */
-  getTitle(transactions,titleRow){
-    let rowData=transactions[titleRow];
-    for (var i = 0; i < rowData.length; i++) {
-      if(rowData[i]){
-        return rowData[i];
-      }
+  mapTransaction(element) {
+    var mappedLine = [];
+
+    mappedLine.push( Banana.Converter.toInternalDateFormat( element[this.colDate]));
+    mappedLine.push( Banana.Converter.toInternalDateFormat( element[this.colDateValuta]));
+    mappedLine.push( ""); // Doc is empty for now
+    element[this.colDetail] == "" ? mappedLine.push(element[this.colDescr]) : mappedLine.push(lement[this.colDetail] + ", " + element[this.colDescr]);
+    let amount = element[this.colAmount];
+    if ( amount.length > 0) {
+       if ( amount[0] === "-") {
+          mappedLine.push( "");
+          amount = amount.replace(/-/g, ''); //remove minus sign
+          mappedLine.push( Banana.Converter.toInternalNumberFormat( amount, "."));
+       } else {
+          mappedLine.push( Banana.Converter.toInternalNumberFormat( amount, "."));
+          mappedLine.push( "");
+       }
     }
+
+    return mappedLine;
+  }
+};
+
+/**
+ * CSV Format 2
+ *
+ * Indea-Me.;330217/01 CHF;;;;
+ * ;;;;;
+ * Ficerieriectudit;;;;;
+ * ;;;;;
+ * Konto-Nr.;Erfassungsdatum;Bezeichnung;Detail;Valutadatum;Betrag
+ * 330217/01;31.03.2023;Plapillumedducipse;;31.03.2023;-1.1
+ * ;;Latipsuceligant;;;
+ * ;;Xxx;01.01.2023  9,5000 %;;
+ * ;;Oluditerunto;0,17- COR;;
+ * ;;Intare Lia: 1202NHK5576738QR81JWWLY4;;;
+ * 330217/01;29.03.2023;Probiresto navivens que�aeps;;31.03.2023;-43.08
+ * ;;D.R.EX. 8,8% Morespeddum;1,80 COR;;
+ * ;;Intare Lia: 8240HXY1054264KA73YKVHI3;;;
+ * 330217/01;27.03.2023;I-silique;;27.03.2023;-29.9
+ * ;;Ter Coeperm xxx 42.15.2320;;;
+ * ;;CH3204177224321861564;;;
+ * ;;MERANECT ELIS;;;
+ * ;;INGISTANES 12O;;;
+ * ;;8805 RICHTERSWIL;;;
+ * ;;LOCUTIMULUM AN;;;
+ * ;;IN - FUIT;;;
+ * ;;000000000000000000010071783;;;
+ * ;;TENSAMOX 2531534 NENT 72164 OCUNUNT;;;
+ * ;;TERIUM: SCRO-MAXILLA.EX;;;
+ * ;;BKL64777817724636;;;
+ * ;;Evente lis Subittro;29,90 CHF;;
+ * ;;Intare Lia: MC4813047621;;;
+ */
+
+var ImportCornerBankFormat2 = class ImportCornerBankFormat2 extends ImportUtilities {
+
+  constructor(banDocument){
+    super(banDocument);
+
+    this.colConto = 0;
+    this.colDate = 1;
+    this.colDescr = 2;
+    this.colDetail = 3;
+    this.colDateValuta = 4;
+    this.colAmount= 5;
+  
+    this.currentLength = 6;
+  }
+
+  match(transactions) {
+    if ( transactions.length === 0)
+      return false;
+
+    for (var i=0;i<transactions.length;i++)
+    {
+      var transaction = transactions[i];
+
+      var formatMatched=false;
+      if ( transaction.length  === (this.currentLength)  )
+          formatMatched = true;
+      else
+          formatMatched = false;
+
+      if (formatMatched && transaction[this.colDate].match(/[0-9\.]+/g) && transaction[this.colDate].length === 10)
+          formatMatched = true;
+      else
+          formatMatched = false;
+
+      if ( formatMatched && transaction[this.colDateValuta].match(/[0-9\.]+/g) && transaction[this.colDateValuta].length === 10)
+          formatMatched = true;
+      else
+          formatMatched = false;
+
+      if (formatMatched)
+          return true;
+    }
+
     return false;
   }
+  /** Convert the transaction to the format to be imported */
+  convert(transactions) {
+    var transactionsToImport = [];
 
-  getLanguage(title) {//trovare il title
-    //I check the description field (since in de and nl the date field is the same
-    var lang = "";
-    if(title){
-      if (title === "Elenco movimenti") {
-        lang = "it";
-        return lang;
-      }
-      if (title === "Kontenbewegungen") {
-        lang = "de";
-        return lang;
-      }
+    // Filter and map rows
+    for (var i=0;i<transactions.length;i++)
+    {
+      //We take only the complete rows.
+      var transaction = transactions[i];
+      if ( transaction.length  < (this.colBalance+1) )
+          continue;
+      if ((transaction[this.colDate] && transaction[this.colDate].match(/[0-9\.]+/g)) &&
+        (transaction[this.colDateValuta] && transaction[this.colDateValuta].match(/[0-9\.]+/g))){
+          transactionsToImport.push(this.mapTransaction(transaction));
+        }
     }
-    return lang;
+
+    // Sort rows by date (just invert)
+    transactionsToImport = transactionsToImport.reverse();
+
+    // Add header and return
+    var header = [["Date","DateValue","Doc","Description","Income","Expenses"]];
+    return header.concat(transactionsToImport);
   }
 
-  //The purpose of this function is to let the user specify how to convert the categories
-  postProcessIntermediaryData(intermediaryData) {
-    /** INSERT HERE THE LIST OF ACCOUNTS NAME AND THE CONVERSION NUMBER
-     *   If the content of "Account" is the same of the text
-     *   it will be replaced by the account number given */
-    //Accounts conversion
-    var accounts = {
-      "Raiffeisen Kontokorrent": "1020",
-      Bar: "1000",
+  mapTransaction(element) {
+    var mappedLine = [];
 
-      //...
-    };
-
-    /** INSERT HERE THE LIST OF CATEGORIES NAME AND THE CONVERSION NUMBER
-     *   If the content of "ContraAccount" is the same of the text
-     *   it will be replaced by the account number given */
-
-    //Categories conversion
-    var categories = {
-      "Abschreibung Praxisinnenausbau": "6921",
-      "Abschreibungen mobile Sachanlagen": "6920",
-      "AHV, IV, EO, ALV": "5700",
-      "Aufwand für drittleistungen": "4400",
-      "Ausserordentlicher Aufwand": "8010",
-      "Ausserordentlicher Ertrag": "8000",
-      "Bankkreditzinsaufwand": "6800",
-      "Bankspesen und -gebühren": "6840",
-      "Beiträge/Spenden/Vergabungen": "6520",
-      "Berufliche Vorsorge": "5720",
-      "Bruttoertrag Barverkäufe": "3000",
-      "Bruttoertrag Handel Artikel": "3200",
-      "Büromaterial und Fachliteratur": "6500",
-      "Darlehenszinsaufwand": "6801",
-      "Direkte Einkaufsspesen/Frachten/Transporte": "4070",
-      "Einkauf von Bestandteilen": "4001",
-      "Ertrag Miete/Infrastruktur": "3400",
-      "Erträge aus Postcheck- und Bank": "6850",
-      "Fahrzeugaufwand": "6200",
-      "FAK": "5710",
-      "Gebühren und Abgaben": "6360",
-      "Gründungsaufwand": "6580",
-      "Handelseinkauf": "4200",
-      "inaktiv": "6574",
-      "Informatikaufwand": "6570",
-      "Krankentaggeldversicherung": "5740",
-      "Kursgewinne flüssige Mittel": "6892",
-      "Kursverluste": "6842",
-      "Löhne Handel": "5200",
-      "Löhne Produktion": "5000",
-      "Löhne Verwaltung": "5600",
-      "Materialaufwand / Einkauf von Apparaten": "4000",
-      "Mietaufwand": "6000",
-      "Mobiliar und Einrichtung": "1510",
-      "Nebenkosten": "6030",
-      "Privatanteile Verwaltungsaufwand": "6550",
-      "Quellensteuer": "5790",
-      "Rechts- und Beratungsaufwand": "6530",
-      "Reinigung und Entsorgung": "6460",
-      "Reise- und Repräsentationsaufwand": "6640",
-      "Reparaturen / Unterhalt mobile Sachanlagen": "6100",
-      "Sachversicherungen": "6300",
-      "Skonti": "3290", //Duplicate category name but different numbers: => 3090
-      "Sonstiger Personalaufwand": "5089", //Duplicate category name but different numbers: => 5880
-      "Spesenentschädigungen effektiv": "5820",
-      "Spesenentschädigungen pauschal": "5830",
-      "Steuern": "8900",
-      "Strom/Wasser": "6400",
-      "Telefon/Telefax/Porti/Internet": "6510",
-      "Temporäre Arbeitnehmer": "5900",
-      "Unfallversicherung": "5730",
-      "Werbeaufwand": "6600",
-      "Zinsaufwand Gesellschafter": "6820",
-
-      //...
-    };
-
-    //Apply the conversions
-    for (var i = 0; i < intermediaryData.length; i++) {
-      var convertedData = intermediaryData[i];
-
-      //Convert
-      if (convertedData["ContraAccount"]) {
-        var cleanContraAccount = convertedData["ContraAccount"]
-          .replace(/\(.*/, "")
-          .trim(); //We delete everything between parentheses
-        if (categories.indexOf(cleanContraAccount) > -1)
-          convertedData["ContraAccount"] = categories[cleanContraAccount];
-      }
-
-      if (convertedData["Account"]) {
-        if (accounts.indexOf(convertedData["Account"]) > -1)
-          convertedData["Account"] = accounts[convertedData["Account"]];
-      }
-
-      if (convertedData["_Description2"]) {
-        convertedData["Description"] =
-          convertedData["_Description2"] + ", " + convertedData["Description"];
-      }
+    mappedLine.push( Banana.Converter.toInternalDateFormat(element[this.colDate]));
+    mappedLine.push( Banana.Converter.toInternalDateFormat(element[this.colDateValuta]));
+    mappedLine.push(""); // Doc is empty for now
+    mappedLine.push( element[this.colDescr] + " " + element[this.colDetail]);
+    let amount = element[this.colAmount];
+    if ( amount.length > 0) {
+       if ( amount[0] === "-") {
+          mappedLine.push( "");
+          amount = amount.replace(/-/g, ''); //remove minus sign
+          mappedLine.push( Banana.Converter.toInternalNumberFormat( amount, "."));
+       } else {
+          mappedLine.push( Banana.Converter.toInternalNumberFormat( amount, "."));
+          mappedLine.push( "");
+       }
     }
-  }
 
-  translateHeaderIt(inputRow, convertedRow) {
-    convertedRow["Date"] = Banana.Converter.toInternalDateFormat(inputRow["Data registrazione"],"dd.mm.yy");
-    convertedRow["DateValue"] = Banana.Converter.toInternalDateFormat(inputRow["Data valuta"],"dd.mm.yy");
-    convertedRow["Description"] = inputRow["Descrizione"];
-    convertedRow["_Description2"] = inputRow["Dettaglio"];
-    /* use the Banana.Converter.toInternalNumberFormat to convert to the appropriate number format
-     *  we already have negative amounts in Betrag and don't need the to fill the column Expenses*/
-    convertedRow["Income"] = Banana.Converter.toInternalNumberFormat(inputRow["Importo"],".");
-    //convertedRow["Expenses"] = Banana.Converter.toInternalNumberFormat(inputRow["Importo"], ".");
-    convertedRow["_Balance"] = Banana.Converter.toInternalNumberFormat(inputRow["Saldo"],".");
-    return convertedRow;
-  }
-
-  translateHeaderDe(inputRow, convertedRow) {
-    convertedRow["Date"] = Banana.Converter.toInternalDateFormat(inputRow["Erfassungsdatum"],"dd.mm.yy");
-    convertedRow["DateValue"] = Banana.Converter.toInternalDateFormat(inputRow["Valutadatum"],"dd.mm.yy");
-    convertedRow["Description"] = inputRow["Bezeichnung"];
-    convertedRow["_Description2"] = inputRow["Detail"];
-    /* use the Banana.Converter.toInternalNumberFormat to convert to the appropriate number format
-     *  we already have negative amounts in Betrag and don't need the to fill the column Expenses*/
-    convertedRow["Income"] = Banana.Converter.toInternalNumberFormat(inputRow["Betrag"],".");
-    //convertedRow["Expenses"] = Banana.Converter.toInternalNumberFormat(inputRow["Importo"], ".");
-    convertedRow["_Balance"] = Banana.Converter.toInternalNumberFormat(inputRow["Saldo"],".");
-    return convertedRow;
+    return mappedLine;
   }
 };
 
