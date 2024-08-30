ebe246530daf65d3445b57284939bb4ba5a2fbb0
Joel Mbiye <joembiye@gmail.com>
2024-08-30 08:57:35 +0100

    create viseca one import extension

---
+++ ImportApps/viseca_one_transactions_xls/ch.banana.switzerland.import.viseca.js
@@ -0,0 +1,241 @@
+// Copyright [2024] [Banana.ch SA - Lugano Switzerland]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @id = ch.banana.switzerland.import.viseca
+// @api = 1.0
+// @pubdate = 2024-08-28
+// @publisher = Banana.ch SA
+// @description = Viseca (One) - Import movements .xls (Banana+ Advanced)
+// @description.it = Viseca (One) - Importa movimenti .xls (Banana+ Advanced)
+// @description.en = Viseca (One) - Import movements .xls (Banana+ Advanced)
+// @description.de = Viseca (One) - Bewegungen importieren .xls (Banana+ Advanced)
+// @description.fr = Viseca (One) - Importer mouvements .xls (Banana+ Advanced)
+// @doctype = *
+// @docproperties =
+// @task = import.transactions
+// @outputformat = transactions.simple
+// @inputdatasource = openfiledialog
+// @inputencoding = latin1
+// @inputfilefilter = Text files (*.txt *.csv *.xls);;All files (*.*)
+// @inputfilefilter.de = Text (*.txt *.csv *.xls);;Alle Dateien (*.*)
+// @inputfilefilter.fr = Texte (*.txt *.csv *.xls);;Tous (*.*)
+// @inputfilefilter.it = Testo (*.txt *.csv *.xls);;Tutti i files (*.*)
+// @timeout = -1
+// @includejs = import.utilities.js
+
+/**
+ * 
+ * @param {*} inData transactionsData
+ * @param {*} banDocument accounting file, is present only in tests
+ * @param {*} isTest define if it is a test or not.
+ */
+function exec(inData, banDocument, isTest) {
+
+   var progressBar = Banana.application.progressBar;
+   progressBar.start("Elaborating rows", 1);
+   progressBar.step();
+
+   let banDoc;
+
+   if (!inData)
+      return "";
+
+   if (isTest && !banDocument)
+      return "";
+   else if (isTest && banDocument)
+      banDoc = banDocument;
+   else
+      banDoc = Banana.document;
+
+   var importUtilities = new ImportUtilities(banDoc);
+
+   if (!isTest && !importUtilities.verifyBananaAdvancedVersion())
+      return "";
+
+   convertionParam = defineConversionParam(inData);
+   let transactions = Banana.Converter.csvToArray(inData, convertionParam.separator, convertionParam.textDelim);
+   let transactionsData = getFormattedData(transactions, convertionParam, importUtilities);
+
+   // Viseca Card Payments, this format works with the header names.
+   var visecaFormat = new VisecaFormat();
+   if (visecaFormat.match(transactionsData)) {
+      transactions = visecaFormat.convert(transactionsData);
+      return Banana.Converter.arrayToTsv(transactions);
+   }
+   
+   // Format is unknow, return an error
+   importUtilities.getUnknownFormatError();
+
+   return "";
+}
+
+/**
+ * Viseco (One) Format
+ * Transaktionsliste
+ * Erstellt am 14.07.2024
+ * Trans.-Buchungsdatum zwischen 01.05.24 und 18.08.24
+ * Trans.-CAC-Code ist leer auf Ebene 0
+*/
+function VisecaFormat() {
+
+   /** Return true if the transactions match this format */
+   this.match = function (transactionsData) {
+      if (transactionsData.length === 0)
+         return false;
+
+      for (var i = 0; i < transactionsData.length; i++) {
+         var transaction = transactionsData[i];
+         var formatMatched = true;
+         
+         if (formatMatched && transaction["Date"] && transaction["Date"].length >= 8 &&
+            transaction["Date"].match(/^\d{2}.\d{2}.\d{2}$/))
+            formatMatched = true;
+         else
+            formatMatched = false;
+
+         if (formatMatched)
+            return true;
+      }
+
+      return false;
+   }
+
+   this.convert = function (transactionsData) {
+      var transactionsToImport = [];
+
+      for (var i = 0; i < transactionsData.length; i++) {
+         if (transactionsData[i]["Date"] && transactionsData[i]["Date"].length >= 8 &&
+            transactionsData[i]["Date"].match(/^\d{2}.\d{2}.\d{2}$/)) {
+            transactionsToImport.push(this.mapTransaction(transactionsData[i]));
+         }
+      }
+
+      // Sort rows by date
+      transactionsToImport = transactionsToImport.reverse();
+
+      // Add header and return
+      var header = [["Date", "DateValue", "Doc", "ExternalReference", "Description", "Income", "Expenses"]];
+      return header.concat(transactionsToImport);
+   }
+
+   this.mapTransaction = function (transaction) {
+      let mappedLine = [];
+
+      mappedLine.push(Banana.Converter.toInternalDateFormat(transaction["Date"], "dd.mm.yyyy"));
+      mappedLine.push(Banana.Converter.toInternalDateFormat("", "dd.mm.yyyy"));
+      mappedLine.push("");
+      mappedLine.push("");
+      mappedLine.push(transaction["Description"]);
+      if (transaction["Amount"].match(/^[0-9]/))
+         mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["Amount"], '.'));
+      else
+         mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["Amount"], '.'));
+
+      return mappedLine;
+   }
+}
+
+function defineConversionParam(inData) {
+
+   var convertionParam = {};
+   /** SPECIFY THE SEPARATOR AND THE TEXT DELIMITER USED IN THE CSV FILE */
+   convertionParam.format = "csv"; // available formats are "csv", "html"
+   //get text delimiter
+   convertionParam.textDelim = '\"';
+   // get separator
+   convertionParam.separator = findSeparator(inData);
+
+   convertionParam.headerLineStart = 5;
+   convertionParam.dataLineStart = 6;
+
+   /** SPECIFY THE COLUMN TO USE FOR SORTING
+   If sortColums is empty the data are not sorted */
+   convertionParam.sortColums = ["Date", "Description"];
+   convertionParam.sortDescending = false;
+
+   return convertionParam;
+}
+
+function getFormattedData(inData, convertionParam, importUtilities) {
+   var columns = importUtilities.getHeaderData(inData, convertionParam.headerLineStart); //array
+   var rows = importUtilities.getRowData(inData, convertionParam.dataLineStart); //array of array
+   let form = [];
+
+   let convertedColumns = [];
+
+   convertedColumns = convertHeaderDe(columns);
+
+   //Load the form with data taken from the array. Create objects
+   if (convertedColumns.length > 0) {
+      importUtilities.loadForm(form, convertedColumns, rows);
+      return form;
+   }
+
+   return [];
+}
+
+function convertHeaderDe(columns) {
+   let convertedColumns = [];
+
+   for (var i = 0; i < columns.length; i++) {
+      switch (columns[i]) {
+         case "TRANSAKTIONSDATUM":
+            convertedColumns[i] = "Date";
+            break;
+         case "NACHNAME":
+            convertedColumns[i] = "Description";
+            break;
+         case "BRUTTOBETRAG RECHNUNG":
+            convertedColumns[i] = "Amount";
+            break;
+         default:
+            break;
+      }
+   }
+
+   if (convertedColumns.indexOf("Date") < 0) {
+      return [];
+   }
+
+   return convertedColumns;
+}
+
+/**
+ * The function findSeparator is used to find the field separator.
+ */
+function findSeparator(inData) {
+
+   var commaCount = 0;
+   var semicolonCount = 0;
+   var tabCount = 0;
+
+   for (var i = 0; i < 1000 && i < inData.length; i++) {
+      var c = inData[i];
+      if (c === ',')
+         commaCount++;
+      else if (c === ';')
+         semicolonCount++;
+      else if (c === '\t')
+         tabCount++;
+   }
+
+   if (tabCount > commaCount && tabCount > semicolonCount) {
+      return '\t';
+   }
+   else if (semicolonCount > commaCount) {
+      return ';';
+   }
+
+   return ',';
+}
