b724b66b350192ba6c38f19162827cce3f6664db
Joel Mbiye <joembiye@gmail.com>
2024-11-18 17:23:20 +0100

    update: new credit card format expected test results

--- ImportApps/pf_import_bank_statement_csv/test/ch.banana.switzerland.import.postfinance.test.js
+++ ImportApps/pf_import_bank_statement_csv/test/ch.banana.switzerland.import.postfinance.test.js
@@ -70,6 +70,7 @@ TestImportPostfinanceTrans.prototype.testImport = function () {
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format4_20230509.csv");
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format5_20230901.csv");
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format1_CreditCard_20230929.csv");
+    fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format2_CreditCard_20241118.csv");
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format6_20240215-DE.csv");
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format6_20240222-IT.csv");
     fileNameList.push("file:script/../test/testcases/csv_postfinance_example_format6_20240226-FR.csv");
