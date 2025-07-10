5e13b0adfd01d54cf6efb47a10e47ed1a51ab547
Joel Mbiye <joembiye@gmail.com>
2025-07-10 20:49:57 +0100

    fix: format 3 file name

--- ImportApps/blkb_import_bank_statement_csv/test/ch.banana.switzerland.import.blkb.test.js
+++ ImportApps/blkb_import_bank_statement_csv/test/ch.banana.switzerland.import.blkb.test.js
@@ -62,9 +62,9 @@ TestImportBLKBTrans.prototype.testImport = function () {
    fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_2012101.csv");
    fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_20080718.csv");
    fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_20121010.csv");
-   fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_20190218.csv");
-   fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_20250709.csv");
+   fileNameList.push("file:script/../test/testcases/csv_blkb_example_format1_20190218.csv");   
    fileNameList.push("file:script/../test/testcases/csv_blkb_example_format2_20160629.csv");
+   fileNameList.push("file:script/../test/testcases/csv_blkb_example_format3_20250709.csv");
 
    var parentLogger = this.testLogger;
    this.progressBar.start(fileNameList.length);
