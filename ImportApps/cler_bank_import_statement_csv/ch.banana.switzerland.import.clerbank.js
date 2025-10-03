4b0b2a4ab9b11de72c178126e40ad494ea467819
PloszajAaron <aaron.ploszaj@banana.ch>
2025-10-03 09:50:02 +0200

    Update Cler import extension

--- ImportApps/cler_bank_import_statement_csv/ch.banana.switzerland.import.clerbank.js
+++ ImportApps/cler_bank_import_statement_csv/ch.banana.switzerland.import.clerbank.js
@@ -1,4 +1,4 @@
-// Copyright [2024] [Banana.ch SA - Lugano Switzerland]
+// Copyright [2025] [Banana.ch SA - Lugano Switzerland]
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 // @id = ch.banana.switzerland.import.clerbank
 // @api = 1.0
-// @pubdate = 2025-01-30
+// @pubdate = 2025-10-02
 // @publisher = Banana.ch SA
 // @description = Cler Bank - Import movements .csv (Banana+ Advanced)
 // @description.it = Cler Bank - Importa movimenti .csv (Banana+ Advanced)
@@ -39,178 +39,227 @@
  */
 function exec(string, isTest) {
 
-    var importUtilities = new ImportUtilities(Banana.document);
- 
-    if (isTest !== true && !importUtilities.verifyBananaAdvancedVersion())
-       return "";
- 
-    let convertionParam = defineConversionParam(string);
- 
-    var transactions = Banana.Converter.csvToArray(string, convertionParam.separator, '"');
-    let transactionsData = getFormattedData(transactions, convertionParam, importUtilities);
- 
-    // Cler Bank Format, this format works with the header names.
-    var clerBankFormat1 = new ClerBankFormat1();
-    if (clerBankFormat1.match(transactionsData)) {
-       transactions = clerBankFormat1.convert(transactionsData);
-       return Banana.Converter.arrayToTsv(transactions);
-    }
- 
-    // Format is unknow, return an error
-    importUtilities.getUnknownFormatError();
- 
-    return "";
- }
- 
- /**
-  * Cler Bank Format
-  *
-  * Data di registrazione;Data valuta;Tipo di ordine;Testo;Importo di addebito (EUR);Importo di accredito (EUR);Saldo (EUR)
-  * 31.10.2022;31.10.2022;Pagamento in Svizzera;Clothing purchase at ABC Boutique;11.32;;5467.92
-  * 31.10.2022;31.10.2022;Trasferimento di conto;Clothing purchase at ABC Boutique;411.04;;5479.24
-  * 31.10.2022;31.10.2022;Pagamento all'estero;Coffee and snacks at Caffeine Delight;1750.00;;5890.28
- */
- function ClerBankFormat1() {
- 
-    /** Return true if the transactions match this format */
-    this.match = function (transactionsData) {
-       if (transactionsData.length === 0)
-          return false;
- 
-       for (var i = 0; i < transactionsData.length; i++) {
-          var transaction = transactionsData[i];
-          var formatMatched = true;
- 
-          if (formatMatched && transaction["Date"] && transaction["Date"].length >= 10 &&
-             transaction["Date"].match(/^\d{2}.\d{2}.\d{4}$/))
-             formatMatched = true;
-          else
-             formatMatched = false;
- 
-          if (formatMatched)
-             return true;
-       }
- 
-       return false;
-    }
- 
-    this.convert = function (transactionsData) {
-       var transactionsToImport = [];
- 
-       for (var i = 0; i < transactionsData.length; i++) {
-          if (transactionsData[i]["Date"] && transactionsData[i]["Date"].length >= 10 &&
-             transactionsData[i]["Date"].match(/^\d{2}.\d{2}.\d{4}$/)) {
-             transactionsToImport.push(this.mapTransaction(transactionsData[i]));
-          }
-       }
- 
-       // Sort rows by date
-       transactionsToImport = transactionsToImport.reverse();
- 
-       // Add header and return
-       var header = [["Date", "DateValue", "Doc", "ExternalReference", "Description", "Income", "Expenses"]];
-       return header.concat(transactionsToImport);
-    }
- 
-    this.mapTransaction = function (transaction) {
-        let mappedLine = [];
-    
-        mappedLine.push(Banana.Converter.toInternalDateFormat(transaction["Date"], "dd.mm.yyyy"));
-        mappedLine.push(Banana.Converter.toInternalDateFormat(transaction["DateValue"], "dd.mm.yyyy"));
-        mappedLine.push("");
-        mappedLine.push("");
-        mappedLine.push(transaction["Description"]);
-        mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["CreditAmount"], '.'));
-        mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["DebitAmount"], '.'));
- 
-       return mappedLine;
-    }
- }
- 
- function defineConversionParam(inData) {
- 
-    var inData = Banana.Converter.csvToArray(inData);
-    var header = String(inData[0]);
-    var convertionParam = {};
-    /** SPECIFY THE SEPARATOR AND THE TEXT DELIMITER USED IN THE CSV FILE */
-    convertionParam.format = "csv"; // available formats are "csv", "html"
-    //get text delimiter
-    convertionParam.textDelim = '"';
-    // get separator
-    if (header.indexOf(';') >= 0) {
-       convertionParam.separator = ';';
-    } else {
-       convertionParam.separator = ',';
-    }
- 
-    /** SPECIFY AT WHICH ROW OF THE CSV FILE IS THE HEADER (COLUMN TITLES)
-    We suppose the data will always begin right away after the header line */
-    convertionParam.headerLineStart = 0;
-    convertionParam.dataLineStart = 1;
- 
-    /** SPECIFY THE COLUMN TO USE FOR SORTING
-    If sortColums is empty the data are not sorted */
-    convertionParam.sortColums = ["Date", "Doc"];
-    convertionParam.sortDescending = false;
- 
-    return convertionParam;
- }
- 
- function getFormattedData(inData, convertionParam, importUtilities) {
-    var columns = importUtilities.getHeaderData(inData, convertionParam.headerLineStart); //array
-    var rows = importUtilities.getRowData(inData, convertionParam.dataLineStart); //array of array
-    let form = [];
- 
-    let convertedColumns = [];
- 
-    convertedColumns = convertHeaderIt(columns);
- 
-    //Load the form with data taken from the array. Create objects
-    if (convertedColumns.length > 0) {
-       importUtilities.loadForm(form, convertedColumns, rows);
-       return form;
-    }
- 
-    return [];
- }
- 
- function convertHeaderIt(columns) {
-    let convertedColumns = [];
- 
-    for (var i = 0; i < columns.length; i++) {
-       switch (columns[i]) {
-          case "Data di registrazione":
-             convertedColumns[i] = "Date";
-             break;
-          case "Data valuta":
-             convertedColumns[i] = "DateValue";
-             break;
-          case "Tipo di ordine":
-             convertedColumns[i] = "Order Type";
-             break;  
-          case "Testo":
-             convertedColumns[i] = "Description";
-             break; 
-          case "Importo di addebito (EUR)":
-          case "Importo di addebito (CHF)":
-             convertedColumns[i] = "DebitAmount";
-             break;
-          case "Importo di accredito (EUR)":
-          case "Importo di accredito (CHF)":
-             convertedColumns[i] = "CreditAmount";
-             break;
-          case "Saldo (EUR)":
-          case "Saldo (CHF)":
-             convertedColumns[i] = "Balance";
-             break;
-          default:
-             break;
-       }
-    }
- 
-    if (convertedColumns.indexOf("Date") < 0) {
-       return [];
-    }
- 
-    return convertedColumns;
- }
+   var importUtilities = new ImportUtilities(Banana.document);
+
+   if (isTest !== true && !importUtilities.verifyBananaAdvancedVersion())
+      return "";
+
+   let convertionParam = defineConversionParam(string);
+
+   var transactions = Banana.Converter.csvToArray(string, convertionParam.separator, '"');
+   let transactionsData = getFormattedData(transactions, convertionParam, importUtilities);
+
+   // Cler Bank Format, this format works with the header names.
+   var clerBankFormat1 = new ClerBankFormat1();
+   if (clerBankFormat1.match(transactionsData)) {
+      transactions = clerBankFormat1.convert(transactionsData);
+      return Banana.Converter.arrayToTsv(transactions);
+   }
+
+   // Format is unknow, return an error
+   importUtilities.getUnknownFormatError();
+
+   return "";
+}
+
+/**
+ * Cler Bank Format
+ *
+ * Data di registrazione;Data valuta;Tipo di ordine;Testo;Importo di addebito (EUR);Importo di accredito (EUR);Saldo (EUR)
+ * 31.10.2022;31.10.2022;Pagamento in Svizzera;Clothing purchase at ABC Boutique;11.32;;5467.92
+ * 31.10.2022;31.10.2022;Trasferimento di conto;Clothing purchase at ABC Boutique;411.04;;5479.24
+ * 31.10.2022;31.10.2022;Pagamento all'estero;Coffee and snacks at Caffeine Delight;1750.00;;5890.28
+*/
+function ClerBankFormat1() {
+
+   /** Return true if the transactions match this format */
+   this.match = function (transactionsData) {
+      if (transactionsData.length === 0)
+         return false;
+
+      for (var i = 0; i < transactionsData.length; i++) {
+         var transaction = transactionsData[i];
+         var formatMatched = true;
+
+         if (formatMatched && transaction["Date"] && transaction["Date"].length >= 10 &&
+            transaction["Date"].match(/^\d{2}.\d{2}.\d{4}$/))
+            formatMatched = true;
+         else
+            formatMatched = false;
+
+         if (formatMatched)
+            return true;
+      }
+
+      return false;
+   }
+
+   this.convert = function (transactionsData) {
+      var transactionsToImport = [];
+
+      for (var i = 0; i < transactionsData.length; i++) {
+         if (transactionsData[i]["Date"] && transactionsData[i]["Date"].length >= 10 &&
+            transactionsData[i]["Date"].match(/^\d{2}.\d{2}.\d{4}$/)) {
+            transactionsToImport.push(this.mapTransaction(transactionsData[i]));
+         }
+      }
+
+      // Sort rows by date
+      transactionsToImport = transactionsToImport.reverse();
+
+      // Add header and return
+      var header = [["Date", "DateValue", "Doc", "ExternalReference", "Description", "Income", "Expenses"]];
+      return header.concat(transactionsToImport);
+   }
+
+   this.mapTransaction = function (transaction) {
+      let mappedLine = [];
+
+      mappedLine.push(Banana.Converter.toInternalDateFormat(transaction["Date"], "dd.mm.yyyy"));
+      mappedLine.push(Banana.Converter.toInternalDateFormat(transaction["DateValue"], "dd.mm.yyyy"));
+      mappedLine.push("");
+      mappedLine.push("");
+      mappedLine.push(transaction["Description"]);
+      mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["CreditAmount"], '.'));
+      mappedLine.push(Banana.Converter.toInternalNumberFormat(transaction["DebitAmount"], '.'));
+
+      return mappedLine;
+   }
+}
+
+function defineConversionParam(inData) {
+
+   var inData = Banana.Converter.csvToArray(inData);
+   var header = String(inData[0]);
+   var convertionParam = {};
+   /** SPECIFY THE SEPARATOR AND THE TEXT DELIMITER USED IN THE CSV FILE */
+   convertionParam.format = "csv"; // available formats are "csv", "html"
+   //get text delimiter
+   convertionParam.textDelim = '"';
+   // get separator
+   if (header.indexOf(';') >= 0) {
+      convertionParam.separator = ';';
+   } else {
+      convertionParam.separator = ',';
+   }
+
+   /** SPECIFY AT WHICH ROW OF THE CSV FILE IS THE HEADER (COLUMN TITLES)
+   We suppose the data will always begin right away after the header line */
+   convertionParam.headerLineStart = 0;
+   convertionParam.dataLineStart = 1;
+
+   /** SPECIFY THE COLUMN TO USE FOR SORTING
+   If sortColums is empty the data are not sorted */
+   convertionParam.sortColums = ["Date", "Doc"];
+   convertionParam.sortDescending = false;
+
+   return convertionParam;
+}
+
+function getFormattedData(inData, convertionParam, importUtilities) {
+   var columns = importUtilities.getHeaderData(inData, convertionParam.headerLineStart); //array
+   var rows = importUtilities.getRowData(inData, convertionParam.dataLineStart); //array of array
+   let form = [];
+
+   let convertedColumns = [];
+
+   convertedColumns = convertHeaderIt(columns);
+   //Load the form with data taken from the array. Create objects
+   if (convertedColumns.length > 0) {
+      importUtilities.loadForm(form, convertedColumns, rows);
+      return form;
+   }
+
+   convertedColumns = convertHeaderFr(columns);
+   //Load the form with data taken from the array. Create objects
+   if (convertedColumns.length > 0) {
+      importUtilities.loadForm(form, convertedColumns, rows);
+      return form;
+   }
+
+   return [];
+}
+
+function convertHeaderFr(columns) {
+   let convertedColumns = [];
+
+   for (var i = 0; i < columns.length; i++) {
+      switch (columns[i]) {
+         case "Date de comptabilisation":
+            convertedColumns[i] = "Date";
+            break;
+         case "Type d'ordre":
+            convertedColumns[i] = "OrderType";
+            break;
+         case "Libellé":
+            convertedColumns[i] = "Description";
+            break;
+         case "Montant débité (CHF)":
+         case "Montant débité (EUR)":
+         case "Montant débité (USD)":
+            convertedColumns[i] = "DebitAmount";
+            break;
+         case "Montant crédité (CHF)":
+         case "Montant crédité (EUR)":
+         case "Montant crédité (USD)":
+            convertedColumns[i] = "CreditAmount";
+            break;
+         case "Solde (CHF)":
+         case "Solde (EUR)":
+         case "Solde (USD)":
+            convertedColumns[i] = "Balance";
+            break;
+         default:
+            break;
+      }
+   }
+
+   if (convertedColumns.indexOf("Date") < 0
+      && convertedColumns.indexOf("OrderType") < 0) {
+      return [];
+   }
+
+   return convertedColumns;
+}
+
+function convertHeaderIt(columns) {
+   let convertedColumns = [];
+
+   for (var i = 0; i < columns.length; i++) {
+      switch (columns[i]) {
+         case "Data di registrazione":
+            convertedColumns[i] = "Date";
+            break;
+         case "Data valuta":
+            convertedColumns[i] = "DateValue";
+            break;
+         case "Tipo di ordine":
+            convertedColumns[i] = "OrderType";
+            break;
+         case "Testo":
+            convertedColumns[i] = "Description";
+            break;
+         case "Importo di addebito (EUR)":
+         case "Importo di addebito (CHF)":
+            convertedColumns[i] = "DebitAmount";
+            break;
+         case "Importo di accredito (EUR)":
+         case "Importo di accredito (CHF)":
+            convertedColumns[i] = "CreditAmount";
+            break;
+         case "Saldo (EUR)":
+         case "Saldo (CHF)":
+            convertedColumns[i] = "Balance";
+            break;
+         default:
+            break;
+      }
+   }
+
+   if (convertedColumns.indexOf("Date") < 0
+      && convertedColumns.indexOf("OrderType") < 0) {
+      return [];
+   }
+
+   return convertedColumns;
+}
