75d36cc1e30d6109c32578c097ed0f252312e183
PloszajAaron <aaron.ploszaj@banana.ch>
2025-10-29 12:38:04 +0100

    Fix SmallInvoice import extension
    - Fix language using only document language

--- ImportApps/SmartBusinessPostFinance_csv/ch.banana.switzerland.pf.smartbusinees.import.csv.js
+++ ImportApps/SmartBusinessPostFinance_csv/ch.banana.switzerland.pf.smartbusinees.import.csv.js
@@ -847,10 +847,6 @@ function getLang() {
    var lang = 'en';
    if (this.banDocument)
       lang = this.banDocument.locale;
-   else if (Banana.application.locale)
-      lang = Banana.application.locale;
-   if (lang.length > 2)
-      lang = lang.substr(0, 2);
    return lang;
 }
 
